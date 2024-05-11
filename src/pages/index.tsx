7bab978c0792b71b94589a8c105d022064f2887a
Jonathan Atiene <bemijonathan@gmail.com>
2024-05-11 17:00:16 +0100

    side bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,23 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import {SideBar} from "@/components/blocks";
 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
-      <Alert>
-        <AlertTitle>Heads up!</AlertTitle>
-        <AlertDescription>
-          You can add components and dependencies to your app using the cli.
-        </AlertDescription>
-      </Alert>
+    <div className="flex">
+        <SideBar />
+        <div>
+            <Alert>
+                <AlertTitle>Heads up!</AlertTitle>
+                <AlertDescription>
+                    You can add components and dependencies to your app using the cli.
+                </AlertDescription>
+            </Alert>
+        </div>
     </div>
   );
 }
